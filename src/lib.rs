03c689160718013c012d00582e0981291a48f3c7
lukingcathy <cathyluking@foxmail.com>
2025-08-12 11:11:11 +0800

    将 add_function 模块重命名为 load_extension
    1. 不仅仅是添加新的 scalar_function，也要加载自定义的 fts5 tokenizer

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-mod add_function;
 #[cfg(feature = "build_extension")]
 mod create_extension;
+mod load_extension;
 pub mod pinyin;
 pub mod tokenizer;
 mod utils;
 
 include!(concat!(env!("OUT_DIR"), "/stopword_data.rs"));
 
-use add_function::create_scalar_functions;
+use load_extension::create_scalar_functions;
 use log::LevelFilter;
 use rusqlite::Connection;
 use utils::init_logging;
